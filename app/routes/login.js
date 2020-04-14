e12026c025d176049b26c0384bc449a2e01df819
AllenEsteves <estevesallen@yahoo.com>
2020-04-15 00:05:23 +0800

    second commit

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 
+
 export default class LoginRoute extends Route {
     @action
         pressed(){
-            alert('You login successfully')
+            var user = user;
+ 
+            alert(user);
         }
 }
