55fd2a29ce0c86d5e57e95e002a3bd9142e87366
AllenEsteves <estevesallen@yahoo.com>
2020-04-15 22:07:25 +0800

    Third commit

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,12 +1,15 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
-
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginRoute extends Route {
+    @tracked username;
+
     @action
         pressed(){
-            var user = user;
- 
-            alert(user);
+
+            var username = this.get('username');
+            alert('You login successfully ' + username);
+
         }
 }
